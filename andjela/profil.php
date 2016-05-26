f008bbeaabee45880c12681e5e820e9a1b122407
andjelabai <andobo123@hotmail.com>
2016-05-26 19:45:21 +0200

    andjela commit

--- andjela/profil.php
+++ andjela/profil.php
@@ -97,7 +97,66 @@
 	}
 ?>
 
-<a href="../view/index.php">Nazad</a>
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">    
+
+    <title>LiveTag - Home</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="../css/modal.css" rel="stylesheet">
+    <link href="../css/style.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<div class="navbar navbar-default navbar-fixed-top" role="navigation">
+  <div class="container">
+	<div class="navbar-header">
+	  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+		<span class="sr-only">Toggle navigation</span>
+		<span class="icon-bar"></span>
+		<span class="icon-bar"></span>
+		<span class="icon-bar"></span>
+	  </button>
+	  <a class="navbar-brand" href="../view/index.php">LiveTag</a>
+	</div>
+	<div class="navbar-collapse collapse navbar-right">
+	  <ul class="nav navbar-nav">
+		<li id="newMessage"></li>                              
+		<?php
+			if(($_SESSION['userType'] != 'guest') && ($_SESSION['userType'] != 'admin')) {
+				echo "<li><a href=\"profil.php?userID=" . $_SESSION['userID'] . "\">" . $_SESSION['userName'] . "</a></li>"
+					. "<li><a href=\"../genal/logout.php\">Logout</a></li>";
+			} 
+			if($_SESSION['userType'] == 'guest') {
+				echo "<li><a href=\"../genal/logout.php\">Ulogujte se</a></li>";
+			}
+			if($_SESSION['userType'] == 'admin') {
+				echo "<li><a href=\"../gazda/adminpanel.php\">Admin panel</a></li>" . 
+					"<li><a href=\"../gazda/moderator.php\">Moderator panel</a></li>" . 
+					"<li><a href=\"../genal/logout.php\">Logout</a></li>";
+				}
+		?>                    
+	  </ul>
+	</div><!--/.nav-collapse -->
+  </div>
+</div>
+
+<!-- <a href="../view/index.php">Nazad</a> -->
 
 <form action ="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" enctype="multipart/form-data">
 <div id="sve">
@@ -244,7 +303,7 @@
 		if ($pictures != false) {
 	?>
 		<div id="kompletna">
-			</br><a href="galerija.php?userID=<?php echo $userID ?>">Kompletna galerija korisnika>></a>
+			</br><a id="link" href="galerija.php?userID=<?php echo $userID ?>">Kompletna galerija korisnika>></a>
 		</div>
 	<?php
 		}
