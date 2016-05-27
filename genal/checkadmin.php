bd95ead6ad5a65dab067e7c9e4ca1366e6ca493d
Nalge <aleksandar.genal@gmail.com>
2016-05-27 14:48:14 +0200

    admin mod check update

--- genal/checkadmin.php
+++ genal/checkadmin.php
@@ -1,5 +1,4 @@
 <?php 
-session_start();
 	if(!isset($_SESSION["userType"])||$_SESSION["userType"]!="admin"){
 		header("Location: ../genal/login.php");
 		exit();
--- genal/checkmod.php
+++ genal/checkmod.php
@@ -1,5 +1,4 @@
 <?php 
-session_start();
 	if(!isset($_SESSION["userType"])||$_SESSION["userType"]!="mod"){
 		header("Location: ../genal/login.php");
 		exit();
