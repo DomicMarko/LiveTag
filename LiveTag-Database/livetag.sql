804a8d1a18f886d5f9fca49ab8e32c9bced1923e
Marko Domic <micdo94@outlook.com>
2016-06-02 22:20:22 +0200

    Marko 21. commit

--- LiveTag-Database/livetag.sql
+++ LiveTag-Database/livetag.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 01, 2016 at 01:18 PM
+-- Generation Time: Jun 02, 2016 at 10:19 PM
 -- Server version: 5.6.17
 -- PHP Version: 5.5.12
 
