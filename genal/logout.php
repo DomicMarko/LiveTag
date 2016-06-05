ce089a36e710055fa276c981153b9dd338d59a25
Nalge <aleksandar.genal@gmail.com>
2016-06-05 18:24:58 +0200

    Update logout.php

--- genal/logout.php
+++ genal/logout.php
@@ -1,10 +1,11 @@
+
+<?php 
 	/*	################################################################
 	Autor: Aleksandar Genal 2013/0012, tim Elites
 	Projekat: LiveTag
 	Verzija: 1.0
 	
 	################################################################# */ 
-<?php 
 	session_start();
 	session_destroy();
 	header("Location: login.php")
