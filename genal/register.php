035d41f29c71f943e13557518ebecf5892b19684
Nalge <aleksandar.genal@gmail.com>
2016-06-05 18:25:12 +0200

    Update register.php

--- genal/register.php
+++ genal/register.php
@@ -1,10 +1,11 @@
+
+<?php 
 	/*	################################################################
 	Autor: Aleksandar Genal 2013/0012, tim Elites
 	Projekat: LiveTag
 	Verzija: 1.0
 	
 	################################################################# */ 
-<?php 
 session_start();
 if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"]=true){
   header("Location: ../index.php");
