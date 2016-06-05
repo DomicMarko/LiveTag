8aad9ab05c85068025b2ce3d7637de68b5fcb09b
Nalge <aleksandar.genal@gmail.com>
2016-06-05 17:41:28 +0200

    Zaglavlje7

--- genal/logout.php
+++ genal/logout.php
@@ -1,3 +1,9 @@
+	/*	################################################################
+	Autor: Aleksandar Genal 2013/0012, tim Elites
+	Projekat: LiveTag
+	Verzija: 1.0
+	
+	################################################################# */ 
 <?php 
 	session_start();
 	session_destroy();
