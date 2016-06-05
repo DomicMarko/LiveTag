629dd0680a58b2f391d22a4459d1d6cce5b82574
Nalge <aleksandar.genal@gmail.com>
2016-06-05 17:36:47 +0200

    Zaglavlje1

--- genal/checkadmin.php
+++ genal/checkadmin.php
@@ -1,3 +1,10 @@
+	/*	################################################################
+	Autor: Aleksandar Genal 2013/0012, tim Elites
+	Projekat: LiveTag
+	Verzija: 1.0
+	
+	################################################################# */ 
+
 <?php 
 	if(!isset($_SESSION["userType"])||$_SESSION["userType"]!="admin"){
 		header("Location: ../genal/login.php");
