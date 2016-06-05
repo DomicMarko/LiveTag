ff45518513a22d4426aa95ccee5f73032cbfe1e0
Nalge <aleksandar.genal@gmail.com>
2016-06-05 18:23:12 +0200

    Zaglavljev2

--- genal/checkmod.php
+++ genal/checkmod.php
@@ -1,11 +1,12 @@
+
+
+<?php 
 	/*	################################################################
 	Autor: Aleksandar Genal 2013/0012, tim Elites
 	Projekat: LiveTag
 	Verzija: 1.0
 	
 	################################################################# */ 
-
-<?php 
 	if(!isset($_SESSION["userType"])||$_SESSION["userType"]!="mod"){
 		header("Location: ../genal/login.php");
 		exit();
