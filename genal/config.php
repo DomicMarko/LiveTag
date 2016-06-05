0cab6905fd68419e805053f069433bbbf374711f
Nalge <aleksandar.genal@gmail.com>
2016-06-05 18:29:12 +0200

    Update config.php

--- genal/config.php
+++ genal/config.php
@@ -10,9 +10,9 @@
 /*
  * All database connection variables
  */
-
-define('DB_USER', "root"); // db user
+include("../dbconfig.php");
+/*define('DB_USER', "root"); // db user
 define('DB_PASSWORD', ""); // db password (mention your db password here)
 define('DB_DATABASE', "livetag"); // database name
-define('DB_SERVER', "localhost"); // db server
+define('DB_SERVER', "localhost"); // db server*/
 ?>
