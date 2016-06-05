499732bbcea5790f71458f77c4f02bfe25dfd691
Nalge <aleksandar.genal@gmail.com>
2016-06-05 18:22:54 +0200

    Zaglavljev2

--- genal/checklogin.php
+++ genal/checklogin.php
@@ -1,11 +1,12 @@
+
+
+<?php 
 	/*	################################################################
 	Autor: Aleksandar Genal 2013/0012, tim Elites
 	Projekat: LiveTag
 	Verzija: 1.0
 	
 	################################################################# */ 
-
-<?php 
 session_start();
 	if(!isset($_SESSION["loggedin"])||$_SESSION["loggedin"]!=true){
 		header("Location: ../genal/login.php");
